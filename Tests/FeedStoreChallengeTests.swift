12fdb3ca4cb92c5b8e832ca76450a63583cfe829
Caio Ortu <caio.ortu@avenuecode.com>
2021-03-08 23:02:59 -0300

    Test that retrieve delivers found values on non empty cache

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -6,16 +6,65 @@ import XCTest
 import FeedStoreChallenge
 
 class UserDefaultsFeedStore: FeedStore {
+	private let storeKey: String = "feedStoreKey"
+	private let userDefaults: UserDefaults
+	
+	private struct Cache: Codable {
+		let feed: [CodableFeedImage]
+		let timestamp: Date
+		
+		var localFeed: [LocalFeedImage] {
+			return feed.map { $0.local }
+		}
+	}
+	
+	private struct CodableFeedImage: Codable {
+		private let id: UUID
+		private let description: String?
+		private let location: String?
+		private let url: URL
+		
+		init(_ image: LocalFeedImage) {
+			id = image.id
+			description = image.description
+			location = image.location
+			url = image.url
+		}
+		
+		var local: LocalFeedImage {
+			return LocalFeedImage(id: id, description: description, location: location, url: url)
+		}
+	}
+	
+	init(userDefaults: UserDefaults = .standard) {
+		self.userDefaults = userDefaults
+	}
+	
 	func deleteCachedFeed(completion: @escaping DeletionCompletion) {
 		
 	}
 	
 	func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-		
+		let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
+		do {
+			let data = try JSONEncoder().encode(cache)
+			self.userDefaults.set(data, forKey: self.storeKey)
+			completion(nil)
+		} catch {
+			completion(error)
+		}
 	}
 	
 	func retrieve(completion: @escaping RetrievalCompletion) {
-		completion(.empty)
+		guard let data = userDefaults.data(forKey: storeKey) else {
+			return completion(.empty)
+		}
+		do {
+			let cache = try JSONDecoder().decode(Cache.self, from: data)
+			completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
+		} catch {
+			completion(.failure(error))
+		}
 	}
 }
 
@@ -46,9 +95,9 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 	}
 	
 	func test_retrieve_deliversFoundValuesOnNonEmptyCache() throws {
-//		let sut = try makeSUT()
-//
-//		assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
+		let sut = try makeSUT()
+
+		assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
 	}
 	
 	func test_retrieve_hasNoSideEffectsOnNonEmptyCache() throws {
